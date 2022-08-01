516d67fbb72ed79f988649cd69dac06383f8d62d
Tom Mathews <9562152+Mathews-Tom@users.noreply.github.com>
2022-08-02 00:12:16 +0530

    Refactor code

--- 1-Hand_Tracking/sample.py
+++ 1-Hand_Tracking/sample.py
@@ -9,15 +9,15 @@ cTime = 0
 cap = cv2.VideoCapture(0)
 detector = ht.HandDetector()
 while True:
-    success, img = cap.read()
-    img = detector.findHands(img, draw=True )
-    landmarks = detector.findPosition(img, draw=False)
+    _, frame = cap.read()
+    frame = detector.findHands(frame, draw=True )
+    landmarks = detector.findPosition(frame, draw=False)
     if len(landmarks) != 0:
         print(landmarks[4])
     cTime = time.time()
     fps = 1 / (cTime - pTime)
     pTime = cTime
-    #cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3,
+    #cv2.putText(frame, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3,
      #           (255, 0, 255), 3)
-    cv2.imshow("Image", img)
+    cv2.imshow("Frame", frame)
     cv2.waitKey(1)
